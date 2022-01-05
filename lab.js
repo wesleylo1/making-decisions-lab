fe81dca6ac7db357a9aed110cd66229f47a2b6bb
Wesley Lo <wesleylo1995@gmail.com>
2022-01-05 15:13:28 -0600

    Problem 10 completed

--- lab.js
+++ lab.js
@@ -143,7 +143,9 @@ if (changeMyMind == true) {
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
-//CODE HERE
+if (changeMyMind !== 'pizza') {
+  console.log('true')
+}
 
 
 
